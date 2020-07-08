61b753842a7a0ffdcce162c45d5bbf631edc90ec
Nisheeth Bandaru <nisheeth@outlook.com>
2020-07-08 00:02:04 -0400

    Initial Commit

---
+++ LengthConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LengthConverter
+//
+//  Created by Nisheeth Bandaru on 7/8/20.
+//  Copyright © 2020 Nisheeth Bandaru. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
